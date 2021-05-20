d786404fc5a6cb047f0b43291b84fb903b2e6c80
101603439 <101603439@student.swin.edu.au>
2021-05-20 13:32:09 +1000

    Finished

---
+++ Exercises 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercises_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("What is your name");
+            string YourName =  Console.ReadLine();
+
+            int counter = 0;
+            int counter2 = 1;
+
+            while (counter < 5)
+            {
+                counter2 = counter;
+
+                while (counter2 < 10)
+                {
+                    System.Console.WriteLine(YourName);
+                    counter2 = counter2 +1; 
+                }
+                Console.WriteLine();
+                counter = counter +1;
+            }
+            System.Console.WriteLine("Goodbye" + YourName);
+        }
+    }
+}
